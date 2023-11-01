a1ef8bfc8b6e6f163d289a9487fad00b17848bc8
Fredrik Andersson <fredrik.andersson@molndal.se>
2023-11-01 12:32:43 +0100

    Updated text extraction logic

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
 from flask import Flask, request, render_template, send_file
+import requests
 from bs4 import BeautifulSoup
 from gtts import gTTS
-import requests
+from io import BytesIO
 
 app = Flask(__name__)
 
@@ -12,28 +13,24 @@ def index():
 @app.route('/convert', methods=['POST'])
 def convert():
     doc_url = request.form['doc_url']
-    text = get_text_from_doc(doc_url)
     
-    if text:
-        tts = gTTS(text=text, lang='sv')
-        output_path = 'output.mp3'
-        tts.save(output_path)
+    try:
+        response = requests.get(doc_url)
+        soup = BeautifulSoup(response.text, 'html.parser')
         
-        return send_file(output_path, as_attachment=True)
-    else:
-        return "Kunde inte hämta eller konvertera texten."
-
-def get_text_from_doc(published_doc_url):
-    response = requests.get(published_doc_url)
+        # Anta att vi vill extrahera all text från sidan, du kan anpassa detta baserat på dina behov
+        text_to_convert = soup.get_text()
+        
+        tts = gTTS(text=text_to_convert, lang='sv')
+        
+        mp3_fp = BytesIO()
+        tts.save(mp3_fp)
+        mp3_fp.seek(0)
+        
+        return send_file(mp3_fp, as_attachment=True, download_name="output.mp3", mimetype='audio/mpeg')
     
-    if response.status_code != 200:
-        return None
-    
-    soup = BeautifulSoup(response.text, 'html.parser')
-    text_elements = soup.find_all(['p', 'h1', 'h2', 'h3', 'h4', 'h5', 'h6', 'li'])
-    text = ' '.join([element.get_text() for element in text_elements])
-    
-    return text
+    except Exception as e:
+        return f"Ett fel inträffade: {e}"
 
 if __name__ == '__main__':
     app.run(debug=True)

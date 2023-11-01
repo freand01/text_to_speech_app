d57955aa9dad649cb58a99271168a41f1857081b
Fredrik Andersson <fredrik.andersson@molndal.se>
2023-11-01 12:49:01 +0100

    Uppdaterade app.py och andra filer

--- app.py
+++ app.py
@@ -1,31 +1,40 @@
-from flask import Flask, request, Response
+from flask import Flask, request, Response, render_template
+from io import BytesIO
 from gtts import gTTS
-import io
+import requests
+from bs4 import BeautifulSoup
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return '<form method="POST" action="/convert"><input name="text"><button type="submit">Convert</button></form>'
+    return render_template('index.html')  # Antagande att du har en HTML-fil att rendera
 
 @app.route('/convert', methods=['POST'])
 def convert():
-    text = request.form['text']
+    doc_url = request.form['doc_url']
     
-    # Skapa en TTS från texten
-    tts = gTTS(text)
-    
-    # Skapa en BytesIO buffer att spara mp3-filen i
-    mp3_fp = io.BytesIO()
-    tts.save(mp3_fp)
-    mp3_fp.seek(0)
-    
-    return send_file(
-        mp3_fp, 
-        as_attachment=True, 
-        download_name='speech.mp3', 
-        mimetype='audio/mp3'
-    )
+    try:
+        # Hämta och konvertera text härifrån
+        response = requests.get(doc_url)
+        response.raise_for_status()
+        
+        soup = BeautifulSoup(response.text, 'html.parser')
+        text = soup.get_text()
+        
+        mp3_fp = BytesIO()
+        tts = gTTS(text=text, lang='en')  # eller vilket språk du föredrar
+        tts.save(mp3_fp)
+        mp3_fp.seek(0)
+        
+        return send_file(
+            mp3_fp,
+            as_attachment=True,
+            download_name='output.mp3',
+            mimetype='audio/mpeg'
+        )
+    except Exception as e:
+        return f"Ett fel inträffade: {str(e)}"
 
 if __name__ == '__main__':
     app.run(debug=True)
